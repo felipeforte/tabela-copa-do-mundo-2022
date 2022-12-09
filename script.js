fc9cb6ae99b711dcb9454388c33c172bf137d01f
felipeforte <64996043+felipeforte@users.noreply.github.com>
2022-12-09 14:09:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ const paises = ["Catar", "Equador", "Holanda", "Senegal",
 "Brasil", "Camarões", "Suíça", "Sérvia",
 "Coreia do Sul", "Gana", "Portugal", "Uruguai"];
 
+//Inicialização do vetor abrev com todos as abreviações das seleções na ordem do vetor paises
+const abrev = ["QAT", "ECU", "NED", "SEN",
+"USA", "ENG", "IRN", "WAL",
+"ARG", "KSA", "MEX", "POL",
+"AUS", "DEN", "FRA", "TUN",
+"GER", "CRC", "ESP", "JPN",
+"BEL", "CAN", "CRO", "MAR",
+"BRA", "CMR", "SUI", "SCG",
+"KOR", "GHA", "POR", "URU"]
+
 // Inicializando o vetor grupos com strings contendo o nome dos grupos
 const grupos = ["A","B", "C","D", "E","F", "G","H"]
 
@@ -23,14 +33,16 @@ for (i=0;i<100;i++) {
 	}
 }
 
-//Inicializando vetor "selecoes" contendo cada país organizado em índices
+//Inicializando vetor "selecoes" para conter cada seleção organizada em índices
 var selecoes = [];
+
+//Inicializando objetos contendo dados de gols, pontos, etc. para cada seleção dentro do vetor "selecoes"
 var n = 0;
 for (i=0;i<paises.length;i++) {
-	//Inicializando objetos contendo dados de gols, pontos, etc. para cada país dentro do vetor "selecoes"
 	selecoes[i] = {
 			nome: paises[i],
 			grupo: '',
+			abrev: abrev[i],
 			pontos: 0,
 			saldo: 0,
 			gols: 0,
@@ -44,6 +56,94 @@ for (i=0;i<paises.length;i++) {
 	if (i>0 && i%4 == 0) {
 		n += 1;
 		selecoes[i].grupo = grupos[n];
+	}
+}
+
+// Script para inicializar os inputs dos placares
+var n = 0;
+var qj = 0;
+for (i=0;i<selecoes.length;i++) {
+	var cabeçalho = document.getElementById(`hg${selecoes[i].grupo}`);
+	if (i%4 == 0) {
+		var tabela = document.createElement("table");
+		tabela.className = "jogos-rivais"
+		tabela.id = `rivais${selecoes[i].grupo}`
+		cabeçalho.after(tabela);
+		n += 4;
+	}
+	var tabela = document.getElementById(`rivais${selecoes[i].grupo}`);
+	for (j=i+1;j<n;j++) {
+		var tr = document.createElement("tr");
+
+		var td1 = document.createElement("td");
+		td1.innerHTML = `${selecoes[i].nome}`;
+		var td2 = document.createElement("td");
+		td2.innerHTML = `${selecoes[j].nome}`;
+
+		var inputid1 = `${selecoes[i].abrev}x${selecoes[j].abrev}`;
+		var inputid2 = `${selecoes[j].abrev}x${selecoes[i].abrev}`;
+		var tdinput1 = document.createElement("td");
+		var input1 = document.createElement("input");
+		input1.setAttribute("type", "text");
+		input1.setAttribute("class", "jogocaixa");
+		input1.setAttribute("onkeypress", "return event.charCode >= 48 && event.charCode <= 57");
+		input1.setAttribute("id", inputid1);
+		input1.setAttribute("onkeyup", `comparar(${inputid1}, ${inputid2}, selecoes[${i}], selecoes[${j}], ${qj})`);
+		tdinput1.append(input1);
+
+		var tdinput2 = document.createElement("td");
+		var input2 = document.createElement("input");
+		input2.setAttribute("type", "text");
+		input2.setAttribute("class", "jogocaixa");
+		input2.setAttribute("onkeypress", "return event.charCode >= 48 && event.charCode <= 57");
+		input2.setAttribute("id", inputid2);
+		input2.setAttribute("onkeyup", `comparar(${inputid1}, ${inputid2}, selecoes[${i}], selecoes[${j}], ${qj})`);
+		tdinput2.append(input2);
+		
+		qj += 1;
+		
+		tr.append(td1);
+		tr.append(tdinput1);
+		tr.append(" x ");
+		tr.append(tdinput2);
+		tr.append(td2);
+
+		tabela.append(tr);
+	}
+	
+}
+
+//Função para classificar gols, pontos, saldo
+function classifcar(v1, v2, time1, time2, jogo, tipo) {
+	if (tipo == "apagar") {
+		tipo = -1;
+		var cond1 = jogo.vit1;
+		var cond2 = jogo.vit2;
+		var x = false;
+	} else {
+		var cond1 = v1 > v2;
+		var cond2 = v2 > v1;
+		tipo = 1;
+		var x = true;
+	}
+	console.log(cond1);
+	console.log(cond2);
+	
+	if (cond1) {
+		time1.vit += 1*tipo;
+		time1.pontos += 3*tipo;
+		time2.der += 1*tipo;
+		jogo.vit1 = true&&x;
+	} else if (cond2) {
+		time1.der += 1*tipo;
+		time2.vit += 1*tipo;
+		time2.pontos += 3*tipo;
+		jogo.vit2 = true&&x;
+	} else {
+		time1.emp += 1*tipo;
+		time1.pontos += 1*tipo;
+		time2.emp += 1*tipo;
+		time2.pontos += 1*tipo;
 	}
 }
 
@@ -95,59 +195,28 @@ function comparar(id1, id2, time1, time2, i) {
 		time2.saldo -= (jogo[i].time2 - jogo[i].time1);
 		jogo[i].time1 = 0;
 		jogo[i].time2 = 0;
-		if (jogo[i].vit1) {
-			time1.vit -= 1;
-			time2.der -= 1;
-			jogo[i].vit1 = false;
-		} else if (jogo[i].vit2) {
-			time1.der -= 1;
-			time2.vit -= 1;
-			jogo[i].vit2 = false;
-		} else {
-			time1.emp -= 1;
-			time2.emp -= 1;
-		}
+		classifcar(0, 0, time1, time2, jogo[i], "apagar");
+		
 		// Inicializa dados novos
 		time1.gols += valorid1;
 		time2.gols += valorid2;
+		time1.saldo += (valorid1 - valorid2);
+		time2.saldo += (valorid2 - valorid1);
 		jogo[i].time1 = valorid1;
 		jogo[i].time2 = valorid2;
-		if (valorid1 > valorid2) {
-			time1.vit += 1;
-			time2.der += 1;
-			jogo[i].vit1 = true;
-		} else if (valorid2 > valorid1) {
-			time1.der += 1;
-			time2.vit += 1;
-			jogo[i].vit2 = true;
-		} else {
-			time1.emp += 1;
-			time2.emp += 1;
-		}
-		time1.saldo += (valorid1 - valorid2)
-		time2.saldo += (valorid2 - valorid1)
+		classifcar(valorid1, valorid2, time1, time2, jogo[i], "inserir");
+
 	// Em caso de não haver partida registrada, inicializa dados
 	} else {
 		time1.gols += valorid1;
 		time2.gols += valorid2;
-		time1.saldo += (valorid1 - valorid2)
-		time2.saldo += (valorid2 - valorid1)
+		time1.saldo += (valorid1 - valorid2);
+		time2.saldo += (valorid2 - valorid1);
 		time1.jogos += 1;
 		time2.jogos += 1;
 		jogo[i].time1 = valorid1;
 		jogo[i].time2 = valorid2;
-		if (valorid1 > valorid2) {
-			time1.vit += 1;
-			time2.der += 1;
-			jogo[i].vit1 = true;
-		} else if (valorid2 > valorid1) {
-			time1.der += 1;
-			time2.vit += 1;
-			jogo[i].vit2 = true;
-		} else {
-			time1.emp += 1;
-			time2.emp += 1;
-		}
+		classifcar(valorid1, valorid2, time1, time2, jogo[i], "inserir");
 		jogo[i].partida = true;
 	}
 	// Atualiza tabela dos grupos
